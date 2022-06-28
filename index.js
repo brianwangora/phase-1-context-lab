efda0647cbfe1b11303d9b3cf1799a884df51536
Brian Wangora <brianwangora@gmail.com>
2022-06-28 22:45:07 +0300

    commit tree 5e15563b65d983b598185e8a1cf72a2ccb68ec4f

--- index.js
+++ index.js
@@ -1,4 +1,53 @@
-/* Your Code Here */
+function createEmployeeRecord(array) {
+    return {
+        firstName: `${array[0]}`,
+        familyName: `${array[1]}`,
+        title: `${array[2]}`,
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+function createEmployeeRecords (nestedArray) {
+    return nestedArray.map(function(array){return createEmployeeRecord(array)})    
+}
+
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split('')
+    this.timeInEvents.push({
+        type: "TimeIn", 
+        hour: parseInt(hour, 10), 
+        date,
+    })
+    return this
+}
+
+function createTimeOutEvent (dateStamp) {
+    let [date, hour] = dateStamp.split('')
+    this.timeInEvents.push({
+        type: "TimeOut", 
+        hour: parseInt(hour, 10), 
+        date,
+    })
+    return this
+}
+
+function hoursWorkedOnDate (employeeRecord, dateWorked) {
+    let timeIn = employeeRecord.timeInEvents.find(function(event) {
+        return event.date === dateWorked
+    })
+    let timeOut = employeeRecord.timeOutEvents.find(function(event) {
+        return event.date === dateWorked
+    })
+    return ((timeOut.hour - timeIn.hour) / 100)
+}
+
+function wagesEarnedOnDate (dateWorked) {
+    let wagesEarned = hoursWorkedOnDate.call(this, dateWorked)
+        * this.payPerHour
+    return parseFloat(wagesEarned.toString())
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +70,14 @@ const allWagesFor = function () {
     return payable
 }
 
+let findEmployeeByFirstName = function (srcArray, firstName) {
+    return srcArray.find(function(rec){
+        return rec.firstName === firstName
+    })
+}
+
+let calculatePayroll = function(employeeRecords) {
+    return employeeRecords.reduce(function(memo, rec) {
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
